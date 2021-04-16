02384b1d886a0ef067dc9764aca66bda10a4e8a3
fcventura02 <filipecvr4@gmail.com>
2021-04-16 18:26:23 -0300

    update projects

--- frontend/src/pages/Projects/index.tsx
+++ frontend/src/pages/Projects/index.tsx
@@ -13,33 +13,35 @@ export default function Projects() {
     console.log(projecs)
     return (
         <main className="containner">
-            <header className="header-project">
-                <h2>
-                    Olá, vamos passear um pouco pelos meus projetos.
+            <div className="contain">
+                <header className="header-project">
+                    <h2>
+                        Olá, vamos passear um pouco pelos meus projetos.
                 </h2>
-                <p>
-                    Caso se interesse por algum projeto clique em visitar para ir até a página.
+                    <p>
+                        Caso se interesse por algum projeto clique em visitar para ir até a página.
                 </p>
-            </header>
-            <section className="cards_projects">
-                {projecs.map((project: Project) => (
-                    <div key={project.id} className="card_project">
-                        <img src="https://i.imgur.com/zHGvk1q.png" alt="DS Delivery" />
-                        <div className="card_project-content" >
+                </header>
+                <section className="cards_projects">
+                    {projecs.map((project: Project) => (
+                        <div key={project.id} className="card_project">
+                            <img src="https://i.imgur.com/zHGvk1q.png" alt="DS Delivery" />
+                            <div className="card_project-content" >
 
-                            <h2 className="card_projects-title">{project.name}</h2>
-                            <p className="card_project-description">{project.description}</p>
-                            <h3>Tecnologias</h3>
-                            <div className="card_project-technology">
-                                {project.technologies.map((tecnologi: String, key: number) => (
-                                    <span key={key}>{tecnologi + " "}</span>
-                                ))}
+                                <h2 className="card_projects-title">{project.name}</h2>
+                                <p className="card_project-description">{project.description}</p>
+                                <h3>Tecnologias</h3>
+                                <div className="card_project-technology">
+                                    {project.technologies.map((tecnologi: String, key: number) => (
+                                        <span key={key}>{tecnologi + " "}</span>
+                                    ))}
+                                </div>
+                                <Link className="card_project-btn" to={project.link} >VISITAR</Link>
                             </div>
-                            <Link className="card_project-btn" to={project.link} >VISITAR</Link>
                         </div>
-                    </div>
-                ))}
-            </section>
+                    ))}
+                </section>
+            </div>
         </main>
     )
 }
